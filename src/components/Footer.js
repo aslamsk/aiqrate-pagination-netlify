7f1e3bbb41aa76838fac173d0e606863e9d6c7d8
aslamsk <aslamsk777@gmail.com>
2019-12-26 11:52:23 +0530

    first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,93 @@
+import React, { Component } from "react"
+import "./footer.css"
+import '../components/layout.css'
+import '../pages/main.css'
+import { Row, Col} from "react-bootstrap"
+import { MdMail } from "react-icons/md"
+import { 
+    FaLocationArrow,
+    FaFacebook,
+    FaTwitter,
+    FaLinkedin,
+    FaInstagram
+ } from "react-icons/fa"
+import { Link } from "gatsby"
+// import image from '../images/aiqrate-logo-final-blackbg.png'
+export default class Footer extends Component {
+  render() {
+    return (
+      <div className="Container-fluid footer text-light ">
+        <Row className="p-0 mx-0 text-center aqrations-footer">
+          
+          <Link className="col text-light" style={{background:'#419d78'}} to="/Aiqrations/">
+          AIQRATIONS
+          </Link >
+          <Link className="col text-light" style={{background:'#2d3047'}} to="/Aiqrations/">
+          ARTICLES
+          </Link>
+         
+
+        </Row>
+        <Row className="m-0 p-0">
+          <Col>
+            <address>
+              <div className="col-md-8 align-self-center quick-link-header">
+                <div className="col-md-12 foundation text-light">
+                <div>
+                  <Link to="/" >
+                  {/* <img style={{ margin: 0, height:'40px' }} src={image} alt="logo" className="logo" /> */}
+                  </Link>
+                </div>
+                  <div className="col-md-12">
+                    <ul>
+                      <small>
+                        <li>
+                          
+                          <Link to="">
+                            <MdMail /> consult@aiqrate.ai
+                          </Link>
+                        </li>
+                        <li>
+                          
+                          <FaLocationArrow /> Bangalore | Delhi | Hyderabad
+                        </li>
+                        <li className="text-center">
+                          Privacy Policy | Terms of Use
+                        </li>
+                      </small>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </address>
+          </Col>
+          <Col>
+                <h5 className="align-self-center quick-link-header">QUICK LINKS</h5>
+                <ul className="footer-quick_links">
+                  <small>
+                    <li className="quick-links">
+                        <Link to="">AIQRATE Engagement Model  </Link>
+                        <Link to="">AIQRATIONS  </Link>
+                        <Link to="">AIQRATE Team   </Link>
+                        <Link to="">Contact Us </Link>
+                    </li>
+                    </small>
+                </ul>
+                <ul className="align-self-center d-flex  mt-0">
+                    <li className="d-flex text-dark footer-Icons">
+                        <Link to=""> <FaFacebook /> </Link>
+                        <Link to=""> <FaTwitter /> </Link>
+                        <Link to=""> <FaLinkedin /> </Link>
+                        <Link to=""> <FaInstagram /> </Link>
+                    </li>
+                </ul>
+          </Col>
+        </Row>
+        <Row  className="m-0 p-0" style={{background:'#419d78'}}>
+        <p className="align-self-center my-0"> <small>  © {new Date().getFullYear()}, <span>AIQRATE ADVISORY & CONSULTING PRIVATE LIMITED</span></small></p>
+
+        </Row>
+      </div>
+    )
+  }
+}
---
+++ src/components/layout.js
@@ -0,0 +1,69 @@
+/**
+ * Layout component that queries for data
+ * with Gatsby's StaticQuery component
+ *
+ * See: https://www.gatsbyjs.org/docs/static-query/
+ */
+
+import React from "react"
+import PropTypes from "prop-types"
+import { StaticQuery, graphql } from "gatsby"
+import '../components/layout.css'
+import '../components/header.css'
+import '../components/footer.css'
+import '../components/styles.css'
+import Typography from "typography"
+import moragaTheme from "typography-theme-moraga"
+import { Provider as GridProvider } from "griding"
+import Footer from './Footer'
+import Header from "./header"
+import '../pages/main.css'
+import { Container } from "react-bootstrap"
+// import * as S from "../components/styles.css"
+
+const typography = new Typography(moragaTheme)
+typography.injectStyles()
+
+const Layout = ({ children }) => (
+  <StaticQuery
+    query={graphql`
+      query SiteTitleQuery {
+        site {
+          siteMetadata {
+            title
+          }
+        }
+      }
+    `}
+    render={data => (
+      <GridProvider>
+        <>
+        <Header />
+          {/* <main>{children}</main> */}
+          <Container>{children}</Container>
+        <Footer />
+          {/* <S.Footer>
+            See the original article:{" "}
+            <a href="https://blog.significa.pt/advanced-blog-system-in-gatsby-16e0cd6b85ad" target="_blank" rel="noopener noreferrer">
+              Advanced blog system in Gatsby
+            </a>
+            , by{" "}
+            <a
+              href="https://github.com/danilowoz"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              @danilowoz
+            </a>
+          </S.Footer> */}
+        </>
+      </GridProvider>
+    )}
+  />
+)
+
+Layout.propTypes = {
+  children: PropTypes.node.isRequired,
+}
+
+export default Layout
